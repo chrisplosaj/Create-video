b14de6b557cacc299132135b13940266ae5db861
chrisplosaj <chrisp.ideas@gmail.com>
2014-09-10 17:51:42 -0400

    Create-video

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+
+$(document).ready(function() {
+
+
+	$('body').videoBG({
+		position:"fixed",
+		zIndex:0,
+		mp4:'assets/christmas_snow.mp4',
+		ogv:'assets/christmas_snow.ogv',
+		webm:'assets/christmas_snow.webm',
+		poster:'assets/christmas_snow.jpg',
+		opacity:1,
+		fullscreen:true,
+	});
+	
+
+		
+})
